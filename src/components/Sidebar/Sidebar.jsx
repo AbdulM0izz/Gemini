941ca9c4d4d82cf62d5901697aaf4638eee5f08c
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-05-19 18:33:04 +0500

    Gemini Ui Completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
+    const [Extended, Setextended] = useState(false);
   return (
     <div className='sidebar'>
         <div className="top">
-            <img className='menu' src={assets.menu_icon} alt="Menu" />
+            <img onClick={()=> Setextended(prev=> !prev)} className='menu' src={assets.menu_icon} alt="Menu" />
             <div className="new-chat">
                 <img src={assets.plus_icon} alt="plus-icon" />
-                <p>New Chat</p>
+                { Extended?<p>New Chat</p>:null }
             </div>
-            <div className="recent">
+            { Extended ? <div className="recent">
                 <p className="recent-title">Recent</p>
                 <div className="recent-entry">
                     <img src={assets.message_icon} alt="" />
                     <p>Who is Abdul Moiz...</p>
                 </div>
-            </div>
+            </div>: null}
+            
         </div>
         <div className="bottom">
             <div className="bottom-items recent-entry">
                 <img src={assets.question_icon} alt="" />
-                <p>Help</p>
+               {Extended ?<p>Help</p> : null}
             </div>
             <div className="bottom-items recent-entry">
                 <img src={assets.history_icon} alt="" />
-                <p>Activity</p>
+               {Extended ? <p>Activity</p>: null}
             </div>
             <div className="bottom-items recent-entry">
                 <img src={assets.setting_icon} alt="" />
-                <p>Settings</p>
+               {Extended ?  <p>Settings</p> : null}
             </div>
 
         </div>
